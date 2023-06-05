c687efff6b9268e2029ccfaee3e023a7b3b64b57
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-06-05 19:34:48 +0300

    initial commit

---
+++ pages/events/event1.js
@@ -0,0 +1,7 @@
+
+
+export default function event1() {
+  return (
+    <div>event1 page</div>
+  )
+}
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+const page = () => {
+    return (
+        <h1>events  page </h1>
+    )
+}
+
+export default page
